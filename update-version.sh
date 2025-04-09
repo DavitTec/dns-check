41779601b53f6c6ad7ad213073dfaa2f2222ffe9
David Mullins <me@davit.ie>
2025-04-09 23:36:43 +0200

    Fix: update-version.sh issues
    - Correct the shebang/comment in update-version.sh.
    - Update paths to match your test/ directory (not tests/).
    - Sync versions between package.json and script headers.

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# update-version.sh - Update version numbers in dns-check scripts
 new_version=$(node -p "require('./package.json').version")
-sed -i "s/version [0-9.]*$/version $new_version/" src/dns-check.sh
-sed -i "s/version=[0-9.]*$/version=$new_version/" tests/test_dns-check.sh
-git add src/dns-check.sh tests/test_dns-check.sh package.json
+sed -i "s/Version: [0-9.]*$/Version: $new_version/" ./src/dns-check.sh
+sed -i "s/version=[0-9.]*$/version=$new_version/" ./test/test_dns-check.sh
+git add src/dns-check.sh test/test_dns-check.sh package.json
 git commit -m "chore: update version to $new_version"
 echo "Version updated to $new_version"
